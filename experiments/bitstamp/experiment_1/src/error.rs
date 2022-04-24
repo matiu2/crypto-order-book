bb0416d8ff3ae8d9db6fea6c80025b3959339db4
Matthew Sherborne <msherborne@gmail.com>
2022-04-24 12:05:18 +1000

    simplified error type

--- experiments/bitstamp/experiment_1/src/error.rs
+++ experiments/bitstamp/experiment_1/src/error.rs
@@ -28,39 +28,6 @@ pub enum BitstampError {
         message: TMessage,
         source: tokio_tungstenite::tungstenite::Error,
     },
-    /*
-       #[error("Unspported channel type: value: \"{value}\" original error: \"{source:?}\"")]
-       ChannelType { value: String, source: ParseError },
-       #[error("Unspported event type: value: \"{value}\" original error: \"{source:?}\"")]
-       EventType { value: String, source: ParseError },
-       #[error("Unspported Channel name (due to channel type / prefix): value: \"{value}\" original error: \"{source:?}\"")]
-       ChannelNameChannelType { value: String, source: ParseError },
-       #[error("Unspported Channel name (due to channel currency pair / suffix): value: \"{value}\" original error: \"{source:?}\"")]
-       ChannelNamePair { value: String, source: ParseError },
-       #[error("Unspported channel name: value: \"{value}\"")]
-       ChannelName { value: String },
-       #[error("Unable to parse bid prices from order book data: {source}")]
-       OrderBookBids { source: ParseFloatError },
-       #[error("Unable to parse ask prices from order book data: {source}")]
-       OrderBookAsks { source: ParseFloatError },
-       #[error("Unable to parse a micro second timestamp into u64 from order book data: {duration} error: {source:?}")]
-       OrderBookDurationString {
-           duration: String,
-           source: ParseIntError,
-       },
-       #[error("Unable to parse a micro second timestamp from order book data: {duration} error: {source:?}")]
-       OrderBookDuration { duration: u64, source: ParseError },
-       #[error("Unable to convert a duration: {duration} error: {source:?}")]
-       DurationConvert {
-           duration: u64,
-           source: OutOfRangeError,
-       },
-       #[error("JSON Encode message: {message:?} - Error: {source:?}")]
-       EncodeMessage {
-           message: Message,
-           source: serde_json::Error,
-       },
-    */
 }
 
 impl BitstampError {
