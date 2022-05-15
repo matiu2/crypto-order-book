287e413c0044071d1fec262dca157d5f4a315c0f
Matthew Sherborne <msherborne@gmail.com>
2022-05-16 09:07:19 +1000

    Simplified the code by dropping `impl Stream` and using `StreamExt`

--- binance/src/error.rs
+++ binance/src/error.rs
@@ -1,22 +1,17 @@
 use thiserror::Error;
-use tokio_tungstenite::tungstenite::Message;
 use tungstenite::Error as WSError;
 
 #[derive(Error, Debug)]
 pub enum BinanceError {
     #[error("Unable to connect to websocket: url: \"{url}\" error: \"{error:?}\"")]
     Connect { url: String, error: WSError },
-    #[error(
-        "We connected OK, but later got an error while trying to read the next message: {0:?}"
-    )]
-    MessageError(WSError),
+    #[error("We connected OK, but later got an error while trying to read a message: {0:?}")]
+    MessageError(#[from] WSError),
     #[error("Unable to parse json. Error: \"{error:?}\" Original: \"{original}\"")]
     Json {
         error: serde_json::Error,
         original: String,
     },
-    #[error("Expected a Text Message, but got a different type {0}")]
-    MessageType(Message),
     #[error("invalid header (expected {expected:?}, found {found:?})")]
     InvalidHeader { expected: String, found: String },
     #[error("unknown data store error")]
--- binance/src/lib.rs
+++ binance/src/lib.rs
@@ -1,58 +1,42 @@
-use std::task::Poll;
+use std::pin::Pin;
 
 use futures::StreamExt;
 pub mod model;
 use model::Depth;
 use serde_json::de::from_str;
-use tokio::net::TcpStream;
 use tokio_stream::Stream;
-use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 mod error;
 pub use error::BinanceError as Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// A tokio-stream that simply converts the incoming Json to usable structs
-pub struct DepthStream {
-    input: WebSocketStream<MaybeTlsStream<TcpStream>>,
-}
-
-impl Stream for DepthStream {
-    type Item = Result<Depth>;
-
-    fn poll_next(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> Poll<Option<Self::Item>> {
-        let msg = self.input.poll_next_unpin(cx);
-        match msg {
-            Poll::Ready(None) => Poll::Ready(None),
-            // I was going to have this return a stream of Result's and leave logging up to the app, but because it's a small project I'll just log it in the library layer
-            // I'm going to abandon this code now and rewrite it in a proper library and have it return a stream of results
-            Poll::Ready(Some(Ok(Message::Text(msg)))) => {
-                let depth = from_str::<Depth>(&msg).map_err(|error| Error::Json {
-                    error,
-                    original: msg,
-                });
-                Poll::Ready(Some(depth))
-            }
-            Poll::Ready(Some(Ok(weird_message))) => {
-                Poll::Ready(Some(Err(Error::MessageType(weird_message))))
-            }
-            Poll::Ready(Some(Err(err))) => Poll::Ready(Some(Err(Error::MessageError(err)))),
-            Poll::Pending => Poll::Pending,
-        }
-    }
-}
-
 /// Connect to binance and return a new stream
 /// `instrument` should come from binance's instrument list, eg. "ethbtc"
-pub async fn binance_stream(instrument: &str) -> Result<DepthStream> {
+pub async fn binance_stream(
+    instrument: &str,
+) -> Result<Pin<Box<dyn Stream<Item = Result<Depth>> + Send + 'static>>> {
     let url = format!("wss://stream.binance.com:9443/ws/{instrument}@depth20@100ms");
     let (client, _response) = connect_async(&url)
         .await
         .map_err(|error| Error::Connect { url, error })?;
-    Ok(DepthStream { input: client })
+    Ok(Box::pin(client.filter_map(|result| async move {
+        match result {
+            // Incoming message is text; parse it
+            Ok(Message::Text(msg)) => Some(from_str::<Depth>(&msg).map_err(|error| Error::Json {
+                error,
+                original: msg,
+            })),
+            // Filter out and log warnings for non-text messages
+            Ok(unexpected_message) => {
+                log::warn!("Unexpceted message type (not text): {unexpected_message:?}");
+                None
+            }
+            // Convert all errors
+            Err(err) => Some(Err(err.into())),
+        }
+    })))
 }
 
 #[cfg(test)]
